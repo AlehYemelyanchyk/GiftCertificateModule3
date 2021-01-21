37e6569ab8a416679f6c591a2e497a4afa74c970
Aleh Yemelyanchyk <yem1403@gmail.com>
2021-01-21 14:19:23 +0300

    pom.xml - removed tomcat pass and username application-dev.properties - changed the db settings GiftCertificateModule4Application - updated for external server usage.

--- gift-certificate-web/src/main/java/com/epam/esm/giftcertificatemodule4/GiftCertificateModule4Application.java
+++ gift-certificate-web/src/main/java/com/epam/esm/giftcertificatemodule4/GiftCertificateModule4Application.java
@@ -2,13 +2,23 @@ package com.epam.esm.giftcertificatemodule4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity(debug = true)
 @SpringBootApplication()
-public class GiftCertificateModule4Application {
+public class GiftCertificateModule4Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GiftCertificateModule4Application.class, args);
-	}
+    private static final Class<GiftCertificateModule4Application> APPLICATION_CLASS =
+            GiftCertificateModule4Application.class;
+
+    public static void main(String[] args) {
+        SpringApplication.run(APPLICATION_CLASS, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(APPLICATION_CLASS);
+    }
 }
